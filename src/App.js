d81595971ae1198bdb33327fb0f1529ac53fb6d1
HugoLaguna <hugo@experimentality.co>
2021-05-18 05:44:03 -0500

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          EXPERIMENTALITY
         </a>
       </header>
     </div>
